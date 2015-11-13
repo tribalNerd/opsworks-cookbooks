a003087b90621dbdf28f3d35b7bffce3229a016b
tribalNerd <tribalnerd@technerdia.com>
2015-11-13 11:32:32 -0500

    Stack v1.0.0

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -87,62 +87,62 @@ node[:deploy].each do |app_name, deploy|
     end
 
     # Get Wordpress Salts For wp-config.php
-    uri = URI.parse("https://api.wordpress.org/secret-key/1.1/salt/")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Get.new(uri.request_uri)
-    response = http.request(request)
-    keys = response.body
+#    uri = URI.parse("https://api.wordpress.org/secret-key/1.1/salt/")
+#    http = Net::HTTP.new(uri.host, uri.port)
+#    http.use_ssl = true
+#    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#    request = Net::HTTP::Get.new(uri.request_uri)
+#    response = http.request(request)
+#    keys = response.body
 
     # Database Variables: Stack Database or External Database Information
-    if node[:wordpress][:use_stack_database]
+#    if node[:wordpress][:use_stack_database]
         # Database Stack Layer
-        Chef::Log.info('Using Stack Database')
-        db_name = deploy[:database][:database] rescue nil
-        db_user =  deploy[:database][:username] rescue nil
-        db_password = deploy[:database][:password] rescue nil
-        db_host = deploy[:database][:host] rescue nil
-    else
+#        Chef::Log.info('Using Stack Database')
+#        db_name = deploy[:database][:database] rescue nil
+#        db_user =  deploy[:database][:username] rescue nil
+#        db_password = deploy[:database][:password] rescue nil
+#        db_host = deploy[:database][:host] rescue nil
+#    else
         # External DB - Settings Via JSON
-        Chef::Log.info('Using External Database')
-        db_name = node[:wordpress][:db_name] rescue nil
-        db_user = node[:wordpress][:db_user] rescue nil
-        db_password = node[:wordpress][:db_pass] rescue nil
-        db_host = node[:wordpress][:db_host] rescue nil
-    end
+#        Chef::Log.info('Using External Database')
+#        db_name = node[:wordpress][:db_name] rescue nil
+#        db_user = node[:wordpress][:db_user] rescue nil
+#        db_password = node[:wordpress][:db_pass] rescue nil
+#        db_host = node[:wordpress][:db_host] rescue nil
+#    end
 
     # Error Checking
-    raise 'Database Name Is Missing.' if db_name.empty?
-    raise 'Database Username Is Missing.' if db_user.empty?
-    raise 'Database Password Is Missing.' if db_password.empty?
-    raise 'Database Host Name Is Missing.' if db_host.empty?
+#    raise 'Database Name Is Missing.' if db_name.empty?
+#    raise 'Database Username Is Missing.' if db_user.empty?
+#    raise 'Database Password Is Missing.' if db_password.empty?
+#    raise 'Database Host Name Is Missing.' if db_host.empty?
 
     # No Errors, Log Success
-    Chef::Log.info("Database Found: #{db_host}")
+#    Chef::Log.info("Database Found: #{db_host}")
 
     # Setup wp-config.php File
-    template "#{deploy[:deploy_to]}/current/wp-config.php" do
-        source "wp-config.php.erb"
-        mode 0640
-        owner 'root'
-        group httpuser
-        variables(
-            :db_name   => db_name,
-            :db_user   => db_user,
-            :db_pass   => db_pass,
-            :db_host   => db_host,
-            :keys      => (keys rescue nil)
-        )
-    end
+#    template "#{deploy[:deploy_to]}/current/wp-config.php" do
+#        source "wp-config.php.erb"
+#        mode 0640
+#        owner 'root'
+#        group httpuser
+#        variables(
+#            :db_name   => db_name,
+#            :db_user   => db_user,
+#            :db_pass   => db_pass,
+#            :db_host   => db_host,
+#            :keys      => (keys rescue nil)
+#        )
+#    end
 
-    # Setup .htaccess File
-    template "#{deploy[:deploy_to]}/current/.htaccess" do
-        source "htaccess.erb"
-        mode 0660
-        owner 'root'
-        group httpuser
-    end
+#    # Setup .htaccess File
+#    template "#{deploy[:deploy_to]}/current/.htaccess" do
+#        source "htaccess.erb"
+#        mode 0660
+#        owner 'root'
+#        group httpuser
+#    end
 
     # Delete license.txt File
     file "#{deploy[:deploy_to]}/current/license.txt" do
@@ -171,10 +171,10 @@ node[:deploy].each do |app_name, deploy|
     end
 
     # Loopback Cron: Ensures Scheduled Content Is Posted If Not Posted
-    cron 'wordpress' do
-        hour '*'
-        minute '*/15'
-        weekday '*'
-        command "wget -q -O - http://#{deploy[:domains].first}/wp-cron.php?doing_wp_cron >/dev/null 2>&1"
-    end
+#    cron 'wordpress' do
+#        hour '*'
+#        minute '*/15'
+#        weekday '*'
+#        command "wget -q -O - http://#{deploy[:domains].first}/wp-cron.php?doing_wp_cron >/dev/null 2>&1"
+#    end
 end
