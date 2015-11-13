db166332ff36a6becafd405ae485ab5f8bee7353
tribalNerd <tribalnerd@technerdia.com>
2015-11-13 16:33:23 -0500

    Stack v1.0.0

--- virtualhost/metadata.rb
+++ virtualhost/metadata.rb
@@ -1,8 +1,8 @@
-name             "apache2"
+name             "virtualhost"
 description      "Setup Virtual Hosts File For Apache 2"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 maintainer       "AWS OpsWorks"
 license          "Apache 2.0"
 version          "1.0.0"
 
-recipe "apache2::deploy", "Setup Virtual Hosts File For Apache 2"
+recipe "virtualhost::default", "Setup Virtual Hosts File For Apache 2"
