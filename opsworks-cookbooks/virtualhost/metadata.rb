1e647889b929f2ce0e06ad8674a696154859d40f
tribalNerd <tribalnerd@technerdia.com>
2015-11-14 17:22:07 -0500

    v1.0.0

--- opsworks-cookbooks/virtualhost/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             "virtualhost"
-description      "Setup Virtual Hosts File For Apache 2"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-maintainer       "tribalNerd"
-license          "MIT License"
-version          "1.0.0"
-
-#recipe "virtualhost::default", "Setup Virtual Hosts File For Apache 2"
--- opsworks-cookbooks/yum-memcached/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             "memcached"
-description      "Install Memcached"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-maintainer       "AWS OpsWorks"
-license          "Apache 2.0"
-version          "1.0.0"
-
-recipe "memcached::deploy", "Install Memcached"
