2b736027dad29201c1600bf6d8e5faeaeca66ef0
tribalNerd <tribalnerd@technerdia.com>
2015-11-14 00:25:07 -0500

    Stack v1.0.0

--- virtualhost/recipes/default.rb
+++ virtualhost/recipes/default.rb
@@ -5,7 +5,7 @@
 #
 
 # Create VirtualHost From: templates/default
-node[:web_apps][:domains].each do |domain, admin, root|
+node[:web_apps].each do |domains, domain, admin, root|
     template "/etc/httpd/conf.d/web_apps.conf" do
         source "web_apps.conf.erb"
         owner 'root'
