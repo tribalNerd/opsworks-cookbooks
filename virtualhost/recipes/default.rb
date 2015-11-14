cefb6106a5eca59024c4c57f5a97383a6d4a2fd8
tribalNerd <tribalnerd@technerdia.com>
2015-11-13 23:52:15 -0500

    Stack v1.0.0

--- virtualhost/recipes/default.rb
+++ virtualhost/recipes/default.rb
@@ -5,11 +5,18 @@
 #
 
 # Create VirtualHost From: templates/default
-template "/etc/httpd/conf.d/web_apps.conf" do
-    source "web_apps.conf.erb"
-    owner 'root'
-    group 'root'
-    mode 0644
+node[:deploy].each do |app_name, deploy|
+    template "/etc/httpd/conf.d/web_apps.conf" do
+        source "web_apps.conf.erb"
+        owner 'root'
+        group 'root'
+        mode 0644
+        variables(
+            :domain => (deploy[:web_apps][:domain] rescue nil),
+            :admin => (deploy[:web_apps][:admin] rescue nil),
+            :root => (deploy[:web_apps][:root] rescue nil)
+       )
+    end
 end
 
 # Restart Apache
