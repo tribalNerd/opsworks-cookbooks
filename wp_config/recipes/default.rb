e115cff27a3ba0b06c2b77700927378c292d39c9
tribalNerd <tribalnerd@technerdia.com>
2015-11-15 18:19:23 -0500

    v1.0.0

--- wp_config/recipes/default.rb
+++ wp_config/recipes/default.rb
@@ -5,22 +5,22 @@
 #
 
 # Create Maintenance File If Not Found
-template "#{node[:wordpress][:path]}/maintenance.html" do
+template "#{node[:wp_config][:path]}/maintenance.html" do
     source "maintenance.erb"
     mode "0755"
 end
 
 # Delete Current wp-config.php If Found
-file "#{node[:wordpress][:path]}/wp-config.php" do
+file "#{node[:wp_config][:path]}/wp-config.php" do
     action :delete
     backup false
     only_if do
-        File.exists?("#{node[:wordpress][:path]}/wp-config.php")
+        File.exists?("#{node[:wp_config][:path]}/wp-config.php")
     end
 end
 
 # Create wp-config.php File From Template
-template "#{node[:wordpress][:path]}/wp-config.php" do
+template "#{node[:wp_config][:path]}/wp-config.php" do
     source "wp-config.php.erb"
     mode 0640
     owner 'root'
@@ -28,19 +28,19 @@ template "#{node[:wordpress][:path]}/wp-config.php" do
 end
 
 # Delete Garbage wp-config-sample.php If Found
-file "#{node[:wordpress][:path]}/wp-config-sample.php" do
+file "#{node[:wp_config][:path]}/wp-config-sample.php" do
     action :delete
     backup false
     only_if do
-        File.exists?("#{node[:wordpress][:path]}/wp-config-sample.php")
+        File.exists?("#{node[:wp_config][:path]}/wp-config-sample.php")
     end
 end
 
 # Delete maintenance.html If Found
-file "#{node[:wordpress][:path]}/maintenance.html" do
+file "#{node[:wp_config][:path]}/maintenance.html" do
     action :delete
     backup false
     only_if do
-        File.exists?("#{node[:wordpress][:path]}/maintenance.html")
+        File.exists?("#{node[:wp_config][:path]}/maintenance.html")
     end
 end
