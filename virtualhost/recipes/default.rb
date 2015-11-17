04a1816821dba8697bcddafd3c8c94b84f6f479d
tribalNerd <tribalnerd@technerdia.com>
2015-11-17 16:55:27 -0500

    v1.0.0

--- virtualhost/recipes/default.rb
+++ virtualhost/recipes/default.rb
@@ -7,25 +7,30 @@
 # Loop Through Settings: attributes/default.rb
 node[:virutalhost].each do |domain, data|
     # Remove Config If Found
-    file "#{node[:httpd_home]}/#{domain}.conf" do
+    file "#{node[:httpd_home]}/sites-available/#{domain}.conf" do
         action :delete
         only_if do
-            ::File.exists?("#{node[:httpd_home]}/#{domain}.conf")
+            ::File.exists?("#{node[:httpd_home]}/sites-available/#{domain}.conf")
         end
     end
 
     # Create VirtualHost From: templates/default
-    template "#{node[:httpd_home]}/#{domain}.conf" do
+    template "#{node[:httpd_home]}/sites-available/#{domain}.conf" do
         source "virtualhost.conf.erb"
         owner 'root'
         group 'root'
-        mode 0644
+        mode '640'
         variables(
             :port   => data["port"],
             :domain => domain,
             :email  => data["email"],
             :path   => data["path"]
         )
+    end
+
+    # Symlink Available Sites
+    link "#{node[:httpd_home]}/sites-enabled/#{domain}.conf" do
+      to "#{node[:httpd_home]}/sites-available/#{domain}.conf"
     end
 end
 
