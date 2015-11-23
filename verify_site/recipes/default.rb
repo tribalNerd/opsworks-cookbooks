90151303a11b87ce5c6a5b76254c70bc5a78bf0b
tribalNerd <tribalnerd@technerdia.com>
2015-11-23 07:40:52 -0500

    Added Command Chmod

--- verify_site/recipes/default.rb
+++ verify_site/recipes/default.rb
@@ -8,7 +8,7 @@
 node["verify_site"].each do |filename|
     execute "google" do
         command "echo 'google-site-verification: #{filename}.html' > #{node["app_root"]}/#{filename}.html"
-        command "chmod 0755 google-site-verification: #{filename}.html"
+        command "chmod 0755 #{filename}.html"
         action :run
         only_if do
             !::File.exists?("#{node["app_root"]}/#{filename}.html")
